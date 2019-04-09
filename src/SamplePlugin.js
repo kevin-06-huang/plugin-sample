5118284c1255981aba67f60fc54e5e1967af5607
kevin-06-huang <kevin.06.huang@gmail.com>
2019-04-09 15:05:22 -0700

    first commit

---
+++ src/SamplePlugin.js
@@ -0,0 +1,30 @@
+import { FlexPlugin } from 'flex-plugin';
+import React from 'react';
+import CustomTaskListComponent from './CustomTaskListComponent';
+
+const PLUGIN_NAME = 'SamplePlugin';
+
+export default class SamplePlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex-ui') }
+   * @param manager { import('@twilio/flex-ui').Manager }
+   */
+  init(flex, manager) {
+    flex.AgentDesktopView.Panel1.Content.add(
+      <CustomTaskListComponent key="demo-component" />,
+      {
+        sortOrder: -1,
+      }
+    );
+    flex.CRMContainer.defaultProps.uriCallback = (task) => {
+        return task ? "https://www.bing.com/search?q=" + task.attributes.name : "https://bing.com/";
+    }
+  }
+}
